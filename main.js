12b7919e0a5fb9c1f4225ca6a12fe22bfb899950
John Mook <jedwardmook@yahoo.com>
2022-01-24 15:33:56 -0600

    commit tree 1286efc7ba2921826f8b45e697e158eb44bb7d24

--- main.js
+++ main.js
@@ -3,7 +3,66 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal")
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM has loaded.")
+  
+  
+  clickListener()
+  
+})
+
+errorModal.classList.add("hidden")
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
+
+removeActivated()
+function removeActivated(){
+  document.addEventListener('click',e)
+    if(e.target.classList === 'activated-heart'){
+      e.target.classList.remove("activated-heart") 
+}
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph'){
+      console.log("Found!")
+      mimicServerCall()
+      .then(resp => {
+        event.target.classList.add("activated-heart")
+      })
+      .catch(error => {
+        errorModal.classList.remove("hidden")
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+      })
+  }})
+}
+
+//heart.addEventListener('click', cb(heart))
+
+//function cb(heart) {
+  //console.log(heart.target)
+  //mimicServerCall()
+  //.then(() => {
+    //if (heart.target.innerText === EMPTY_HEART){
+      //heart.target.classList.add('activated-heart')
+      //heart.target.innerText = FULL_HEART
+    //}
+    //else {
+      //heart.target.classList.remove('activated-heart')
+      //heart.target.innerText = EMPTY_HEART
+    //}
+  //})
+  //.catch(() => {
+    
+  //})
+//};
 
 
 
